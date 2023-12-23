6ab193e5f9a92ca31e3f55ee3b38ad80c204f4f0
Nikita Khodakovsky <bednayadusha@gmail.com>
2023-12-23 15:01:33 +0200

    Change loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,7 +5,7 @@ import styles from './Loader.module.scss'
 export function Loader({ style, className }: HTMLAttributes<HTMLDivElement>) {
 	return (
 		<Wrapper classNames={[className, styles.wrap]} style={style}>
-			<div className={styles.ldsDualRing} />
+			<div className={styles.loader} />
 		</Wrapper>
 	)
 }
